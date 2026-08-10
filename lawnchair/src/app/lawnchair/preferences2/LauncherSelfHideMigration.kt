b51735638976b9b6e5e4f087494f3196ae7ca4b0
Wolk Tambowskij <wolk.tambowskij@gmail.com>
2026-08-10 20:24:36 +0000

    style: fix spotless formatting in LauncherSelfHideMigration

--- lawnchair/src/app/lawnchair/preferences2/LauncherSelfHideMigration.kt
+++ lawnchair/src/app/lawnchair/preferences2/LauncherSelfHideMigration.kt
@@ -44,8 +44,7 @@ class LauncherSelfHideMigration(private val context: Context) : DataMigration<Pr
         ComponentKey(ComponentName(context, LawnchairLauncher::class.java), Process.myUserHandle()).toString()
     }
 
-    override suspend fun shouldMigrate(currentData: Preferences): Boolean =
-        currentData[appliedForKey] != selfComponentKey
+    override suspend fun shouldMigrate(currentData: Preferences): Boolean = currentData[appliedForKey] != selfComponentKey
 
     override suspend fun migrate(currentData: Preferences): Preferences {
         val hiddenApps = currentData[hiddenAppsKey] ?: emptySet()
