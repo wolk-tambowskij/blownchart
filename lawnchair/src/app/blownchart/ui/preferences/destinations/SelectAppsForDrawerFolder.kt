07c081ab66b94b3fe3f67f654a621a152140f52e
Wolk Tambowskij <wolk.tambowskij@gmail.com>
2026-08-10 05:06:58 +0000

    fix: remove duplicate IconButton import

--- lawnchair/src/app/blownchart/ui/preferences/destinations/SelectAppsForDrawerFolder.kt
+++ lawnchair/src/app/blownchart/ui/preferences/destinations/SelectAppsForDrawerFolder.kt
@@ -21,7 +21,6 @@ import androidx.compose.material3.Checkbox
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
